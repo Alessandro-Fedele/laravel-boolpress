64efa623dd82a228214ca7b744b576c160ae6958
Alessandro-Fedele <al.fedele@outlook.it>
2022-01-17 16:52:51 +0100

    component App.vue

---
+++ resources/js/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      title: "Vue page",
+    };
+  },
+};
+</script>
+
+<style>
+</style>
